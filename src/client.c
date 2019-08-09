ee9e9ed8fdbff842b649fca8c2ce14bb975ab4c8
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-08-09 10:37:12 -0700

    connected to server

--- src/client.c
+++ src/client.c
@@ -114,10 +114,10 @@ int main(int argc, char *argv[])
   urlinfo_t *urlinfo = parse_url(argv[1]);
   sockfd = get_socket(urlinfo->hostname, urlinfo->port);
 
-  if (send_request() {
+  if (send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path) >= 0) {
 
     while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0) {
-  // print the data we got back to stdout
+  
     }
   }
 
